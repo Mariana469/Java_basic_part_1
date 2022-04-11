6baaf0e76fff42dd4b8ec63dbbdf0a57e310af1b
Mariana.Andrusiak <marianka518@gmail.com>
2022-04-11 16:10:23 +0300

    First two tasks of homework for Java Basic Part 1

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class AddTwoNumbers {
+            public static void main (String[] args) {
+// Task 1
+//                int number1 = 34;
+//                int number2 = 74;
+//                int sum = number1 + number2;
+//
+//                System.out.println("Sum: " + sum);
+// Task 2
+//                System.out.println(-5 + 8 * 6);
+//                System.out.println((55+9) % 9);
+//                System.out.println(20 + -3*5 / 8);
+//                System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+
+            }
+        }
+
