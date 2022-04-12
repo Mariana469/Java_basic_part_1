98e25cccaedbff6156ef99c81500f8e7807c81dd
Mariana.Andrusiak <marianka518@gmail.com>
2022-04-12 13:38:32 +0300

    Added first three tasks of my homework

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,51 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+            public static void main (String[] args) {
+// Task 1
+                /*int number1 = 34;
+                int number2 = 74;
+                int sum = number1 + number2;
+
+                System.out.println("Sum: " + sum);*/
+// Task 2
+                //System.out.println(-5 + 8 * 6);
+                //System.out.println((55+9) % 9);
+                //System.out.println(20 + -3 * 5 / 8);
+                //System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+//  Task 3
+                /*Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter first number: ");
+                int firstNumber = scanner.nextInt();
+                System.out.println("Enter second number: ");
+                int secondNumber = scanner.nextInt();*/
+
+                //System.out.println(firstNumber + " + " + secondNumber + " = " + addNumbers(firstNumber, secondNumber));
+                //System.out.println(firstNumber + " * " + secondNumber + " = " + multiplyNumbers(firstNumber, secondNumber));
+                //System.out.println(firstNumber + " / " + secondNumber + " = " + divideNumbers(firstNumber, secondNumber));
+                //System.out.println(firstNumber + " - " + secondNumber + " = " + subtractNumbers(firstNumber, secondNumber));
+                //System.out.println(firstNumber + " mod " + secondNumber + " = " + remainderFromNumbers(firstNumber, secondNumber));
+
+           }
+                /*public static int addNumbers ( int firstNumber, int secondNumber){
+                 return firstNumber + secondNumber;
+                }*/
+                /*public static int multiplyNumbers ( int firstNumber, int secondNumber){
+                 return firstNumber * secondNumber;
+                }*/
+                /*public static int divideNumbers ( int firstNumber, int secondNumber){
+                 return firstNumber / secondNumber;
+                }*/
+                /*public static int subtractNumbers ( int firstNumber, int secondNumber){
+                 return firstNumber - secondNumber;
+                }*/
+                /*public static int remainderFromNumbers ( int firstNumber, int secondNumber){
+                 return firstNumber % secondNumber;
+                }*/
+
+
+
+}
+
+
