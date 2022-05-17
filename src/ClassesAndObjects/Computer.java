983755ca1ba2415670ff2a944a2dd2b92692173f
Mariana.Andrusiak <marianka518@gmail.com>
2022-05-17 20:07:47 +0300

    Task 2 done

---
+++ src/ClassesAndObjects/Computer.java
@@ -0,0 +1,7 @@
+public class Computer {
+    String Brand;
+    int ScreenSize;
+    public (String brand, int ScreenSize, int CoresNumber, String Type) {
+     //   Computer Obj1  = new Computer();
+    }
+}
