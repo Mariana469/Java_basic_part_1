b899a0451f0589219e79cf2d033d75bee8bab6fa
Mariana.Andrusiak <marianka518@gmail.com>
2022-05-18 11:40:36 +0300

    Finished third task from Part 2 Java Basics

---
+++ src/Task3FromPart2/Main.java
@@ -0,0 +1,8 @@
+package Task3FromPart2;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle myRect = new Rectangle();
+        myRect.area(40,50);
+    }
+}
---
+++ src/Task3FromPart2/Rectangle.java
@@ -0,0 +1,12 @@
+package Task3FromPart2;
+
+public class Rectangle {
+        int width;
+        int height;
+
+
+                void area ( int width, int height){
+                        System.out.println(width * height);
+                }
+        }
+
