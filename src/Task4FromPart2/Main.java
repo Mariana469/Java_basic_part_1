325ba4b6d9772f188dceb9a747c21acece182efe
Mariana.Andrusiak <marianka518@gmail.com>
2022-05-18 12:00:54 +0300

    Added 4 Task from Java Part 2

---
+++ src/Task4FromPart2/Main.java
@@ -0,0 +1,8 @@
+package Task4FromPart2;
+
+public class Main {
+    public static void main(String[] args){
+        NumberHolder myNum1 = new NumberHolder();
+        myNum1.printOnScreen(10, 0.2f);
+    }
+}
---
+++ src/Task4FromPart2/NumberHolder.java
@@ -0,0 +1,11 @@
+package Task4FromPart2;
+
+public class NumberHolder {
+    public int anInt;
+    public float aFloat;
+
+    void printOnScreen(int anInt, float aFloat){
+        System.out.println("My int is: " + anInt);
+        System.out.println("My float is " + aFloat);
+    }
+}
